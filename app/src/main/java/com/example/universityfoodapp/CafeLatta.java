e830d1592543194c44b81024fc8017cbcb510aa5
Prisha1611 <prishapatel16@gmail.com>
2024-01-31 22:30:57 +0300

    registration done

---
+++ app/src/main/java/com/example/universityfoodapp/CafeLatta.java
@@ -0,0 +1,14 @@
+package com.example.universityfoodapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CafeLatta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cafe_latta);
+    }
+}
---
+++ app/src/main/java/com/example/universityfoodapp/MainPage.java
@@ -0,0 +1,18 @@
+package com.example.universityfoodapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainPage extends AppCompatActivity {
+    Button getStartedButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+
+    }
+}
